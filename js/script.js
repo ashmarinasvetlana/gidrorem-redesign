929e751146807be67a2733372c90d94173c537b0
Svetlana Ashmarina <sionova2@gmail.com>
2020-09-03 15:01:01 +0300

    Кнопка наверх

--- js/script.js
+++ js/script.js
@@ -24,7 +24,32 @@ $('form').submit(function(event) {
     return false;
 });
 
-
+ 
+$(function() {
+ 
+    $(window).scroll(function() {
+     
+    if($(this).scrollTop() != 0) {
+     
+    $('#toTop').fadeIn();
+     
+    } else {
+     
+    $('#toTop').fadeOut();
+     
+    }
+     
+    });
+     
+    $('#toTop').click(function() {
+     
+    $('body,html').animate({scrollTop:0},800);
+     
+    });
+     
+    });
+     
+     
 
 
   
