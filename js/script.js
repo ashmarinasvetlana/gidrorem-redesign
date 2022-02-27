a5f0ab234db06f82c314eed30f23de8f2fdf36a2
Svetlana Ashmarina <sionova2@gmail.com>
2022-02-27 12:46:34 +0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,67 +1,137 @@
+window.addEventListener('DOMContentLoaded', function () {
+    AOS.init({
+
+  duration: 1200,
+disable: 'mobile'
+
+		});
+
+		var swiper = new Swiper('.swiper-container', {
+			slidesPerView: '3',
+			spaceBetween: 10,
+			autoHeight: true,
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+		});
+        
+    const form = document.querySelectorAll('form'),
+        inputs = document.querySelectorAll('input'),
+        alertSend = document.querySelector('.alert'),
+        alertFail = document.querySelector('.fail');
 
 
-$("a[href^='#']").click(function(){
-    var _href = $(this).attr("href");
-    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-    return false;
-});
+    /*const path = {
+        feedbackForm: './server.php'
+    };*/
 
-$('input[type="tel"]').mask("+7 (999) 999-99-99");
+    const postData = async (url, data) => {
+        let res = await fetch(url, {
+            method: "POST",
+            body: data
+        });
+
+        return await res.text();
+    };
+
+    const clearInputs = () => {
+        inputs.forEach(item => {
+            item.value = '';
+        });
+    };
+
+    form.forEach(item => {
+        item.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const formData = new FormData(item);
+
+            postData('./smail.php', formData)
+                .then(res => {
+
+                    console.log(res);
+
+                    alertSend.style.display = 'block';
+
+                })
+                .catch(() => {
+
+                    alertFail.style.display = 'block';
+
+                })
 
 
-$('form').submit(function(event) {
-    event.preventDefault();
-    $.ajax({
-        type: "POST",
-        url: "mail.php",
-        data: $(this).serialize()
-    }).done(function() {
-        $(this).find("input").val("");
-        alert("Сообщение успешно отправлено, Мы Вам перезвоним");
-        $("form").trigger("reset");
-        $(".popup").hide();
+                .finally(() => {
+                    clearInputs();
+                    setTimeout(() => {
+                        alertSend.style.display = 'none';
+                        alertFail.style.display = 'none';
+                    }, 3000);
+
+                });
+        });
+
+
+
     });
-    return false;
-});
 
- 
-$(function() {
- 
-    $(window).scroll(function() {
-     
-    if($(this).scrollTop() != 0) {
-     
-    $('#toTop').fadeIn();
-     
+const toTop = document.getElementById('toTop');
+
+window.onscroll = function () {
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrolled > 100) {
+        toTop.style.display = 'block';
     } else {
-     
-    $('#toTop').fadeOut();
-     
+        toTop.style.display = 'none';
     }
-     
-    });
-     
-    $('#toTop').click(function() {
-     
-    $('body,html').animate({scrollTop:0},800);
-     
-    });
-     
-    });
- //чтобы функция работала непрерывно при скроле
-window.onscroll = function(){
-    docScroll();
 };
 
-function docScroll() {
-    let windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    let documentHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    let scrolled = (windowScroll/documentHeight)*100;
+toTop.addEventListener('click', function () {
+    var smoothJumpUp = function () {
+        if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+            window.scrollBy(0, -50);
+            setTimeout(smoothJumpUp, 3);
+        }
+    };
 
-    document.getElementById("myBar").style.width = scrolled + '%';
-}
-    
-     
+    smoothJumpUp();
 
+});
 
-  
+// собираем все якоря; устанавливаем время анимации и количество кадров
+const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+    animationTime = 300,
+    framesCount = 20;
+
+anchors.forEach(function (item) {
+    // каждому якорю присваиваем обработчик события
+    item.addEventListener('click', function (e) {
+        // убираем стандартное поведение
+        e.preventDefault();
+
+        // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
+        let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+
+        // запускаем интервал, в котором
+        let scroller = setInterval(function () {
+            // считаем на сколько скроллить за 1 такт
+            let scrollBy = coordY / framesCount;
+
+            // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+            // и дно страницы не достигнуто
+            if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+                // то скроллим на к-во пикселей, которое соответствует одному такту
+                window.scrollBy(0, scrollBy);
+            } else {
+                // иначе добираемся до элемента и выходим из интервала
+                window.scrollTo(0, coordY);
+                clearInterval(scroller);
+            }
+            // время интервала равняется частному от времени анимации и к-ва кадров
+        }, animationTime / framesCount);
+    });
+});
+		
+
+});
